97264ff824954f1946045edb8091c6278f081686
vsSchweitzer <schweitzer.vinicius@gmail.com>
2019-05-20 00:43:17 -0300

    Implement generic parameterization of actions

--- IntelligentAgentsProject/Assets/Scripts/AgentLib/AgentUtilities/AgentInvoker.cs
+++ IntelligentAgentsProject/Assets/Scripts/AgentLib/AgentUtilities/AgentInvoker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Reflection;
 using UnityEngine;
 
@@ -8,15 +9,16 @@ public static class AgentInvoker {
 
 	public static IEnumerator Invoke(IntelligentAgent agent, string action, List<string> parameters) {
 		MethodInfo actionMethod = agent.GetType().GetMethod(action, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+		List<object> genericParameters = ConvertParameterList(actionMethod, parameters);
 		if (actionMethod != null) {
 			if (actionMethod.GetCustomAttribute(typeof(AgentAction), true) != null) {
 				if (actionMethod.ReturnType == typeof(void)) {
-					actionMethod.Invoke(agent, parameters.ToArray());
+					actionMethod.Invoke(agent, genericParameters.ToArray());
 					yield return new List<Percept>();
 				} else if (actionMethod.ReturnType == typeof(List<Percept>)) {
-					yield return (List<Percept>)actionMethod.Invoke(agent, parameters.ToArray());
+					yield return (List<Percept>)actionMethod.Invoke(agent, genericParameters.ToArray());
 				} else if (actionMethod.ReturnType == typeof(IEnumerator)) {
-					CoroutineWithData<List<Percept>> invocationCoroutine = new CoroutineWithData<List<Percept>>(agent, (IEnumerator) actionMethod.Invoke(agent, parameters.ToArray()));
+					CoroutineWithData<List<Percept>> invocationCoroutine = new CoroutineWithData<List<Percept>>(agent, (IEnumerator) actionMethod.Invoke(agent, genericParameters.ToArray()));
 					yield return invocationCoroutine.coroutine;
 					List<Percept> percepts;
 					try {
@@ -37,6 +39,32 @@ public static class AgentInvoker {
 			Debug.LogError("Action \"" + action + "\" on agent \"" + agent.getAgentIdentifier() + "\" was not found.");
 			throw new Exception();
 		}
+	}
+
+	private static List<object> ConvertParameterList(MethodInfo method, List<string> parameters) {
+		List<object> genericParameterList = new List<object>();
+		ParameterInfo[] methodParameters = method.GetParameters();
+		if (methodParameters.Length == parameters.Count) {
+			int i = 0;
+			foreach (ParameterInfo paramInfo in methodParameters) {
+				try {
+					TypeConverter converter = TypeDescriptor.GetConverter(paramInfo.ParameterType);
+					genericParameterList.Add(converter.ConvertFrom(parameters[i]));
+				} catch {
+					Debug.LogError("Method " + method.Name + " has a parameter that can't be converted from a string: " + paramInfo.ParameterType + " " + paramInfo.Name);
+					throw new Exception();
+				}
+				i++;
+			}
+		} else if (methodParameters.Length > parameters.Count) {
+			Debug.Log("Method " + method.Name + " was called with less parameters than it has");
+			throw new Exception();
+		} else if (methodParameters.Length < parameters.Count) {
+			Debug.Log("Method " + method.Name + " was called with more parameters than it has");
+			throw new Exception();
+		}
+
+		return genericParameterList;
 	}
 
 }
