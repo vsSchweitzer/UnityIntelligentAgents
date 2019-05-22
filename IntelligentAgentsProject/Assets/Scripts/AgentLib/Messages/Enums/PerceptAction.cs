80ca93acdac429d4373a9eb0e7f2ddd76ec08794
vsSchweitzer <schweitzer.vinicius@gmail.com>
2019-05-22 16:54:56 -0300

    Implement percept removal

---
+++ IntelligentAgentsProject/Assets/Scripts/AgentLib/Messages/Enums/PerceptAction.cs
@@ -0,0 +1,6 @@
+
+[System.Serializable]
+public enum PerceptAction {
+	ADD,
+	REMOVE
+}
--- IntelligentAgentsProject/Assets/Scripts/AgentLib/Messages/Percept.cs
+++ IntelligentAgentsProject/Assets/Scripts/AgentLib/Messages/Percept.cs
@@ -1,19 +1,43 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 [System.Serializable]
 public class Percept {
 
 	public string percept;
 	public List<string> perceptValues;
+	public string action;
 
 	public Percept(string percept) {
 		this.percept = percept;
-		this.perceptValues = new List<string>();
+		perceptValues = new List<string>();
+		SetAction(PerceptAction.ADD);
+	}
+
+	public Percept(string percept, PerceptAction action) {
+		this.percept = percept;
+		perceptValues = new List<string>();
+		SetAction(action);
 	}
 
 	public Percept(string percept, List<string> perceptValues) {
 		this.percept = percept;
 		this.perceptValues = perceptValues;
+		SetAction(PerceptAction.ADD);
+	}
+
+	public Percept(string percept, List<string> perceptValues, PerceptAction action) {
+		this.percept = percept;
+		this.perceptValues = perceptValues;
+		SetAction(action);
+	}
+
+	public PerceptAction GetActionEnum() {
+		return (PerceptAction)Enum.Parse(typeof(PerceptAction), action);
+	}
+
+	public void SetAction(PerceptAction action) {
+		this.action = action.ToString().ToUpper();
 	}
 
 }
